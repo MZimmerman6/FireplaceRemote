c8a9bc18ad3294e4fcb632af7cdc12d0c8de60d8
Matthew Zimmerman <Zimmerman@Matthews-MacBook-Pro.local>
2012-12-21 02:46:21 -0500

    Initial Commit

---
+++ FireplaceRemote/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FireplaceRemote
+//
+//  Created by Matthew Zimmerman on 12/21/12.
+//  Copyright (c) 2012 Matthew Zimmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
